7eff107fcbad904f68fcca236adc1f11817ad82d
winston4521 <46812129+winston4521@users.noreply.github.com>
2023-12-23 11:13:27 +0000

    Deploying to gh-pages from @ winston4521/goit-react-hw-08-phonebook@303a7f74be7f6d7104301877d9d1de4c36ce9ed9 🚀

---
+++ static/js/765.a2e19e56.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[765],{9765:function(e,o,t){t.r(o);t(2791);var n=t(184);o.default=function(){return(0,n.jsx)("div",{})}}}]);
+//# sourceMappingURL=765.a2e19e56.chunk.js.map
